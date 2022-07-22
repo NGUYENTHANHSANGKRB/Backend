745b90677f838961bcef910522819b1f3b2281da
Nguyen Thanh Sang <vitinhthanhsang365@gmail.com>
2022-07-22 14:03:35 +0700

    Cai dat du an

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,8 +1,23 @@
-exports.create = (req, res) => {
-    res.send({message: "Create handler"});
+const ApiError = require("../api-error");
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
+
+exports.create = async(req, res, next) => {
+    if (!req.body?.name) {
+        return next(new ApiError(400, "Name is required" ));
+    }
+
+    try {
+        const ContactService = new ContactService(MongoDB.client);
+        const document = await ContactService.create(req.body);
+        return res.send(document);
+    } catch (error) {
+        return next(new ApiError(500,"cannot create contact"));
+    }
 };
 
 exports.findAll = (req, res) => {
+
     res.send({message: "findAll handler"});
 };
 
@@ -25,3 +40,4 @@ exports.deleteAll = (req, res) => {
 exports.findAllFavorite = (req, res) => {
     res.send({message: "findAllFavorite handler"});
 };
+
--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const app = require("./app");
 const config = require("./config");
+const MongoDB = require("./utils/mongodb.util");
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+    try {
+        await MongoDB.connect(config.db.uri);
+        console.log("Ket noi thanh cong voi MongoDB!");
+        
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+        });
+
+    } catch (error) {
+        console.log("Khong the ket noi den MongoDB!", error);
+        process.exit();
+    }
+}
+
+startServer();
+
+
 
